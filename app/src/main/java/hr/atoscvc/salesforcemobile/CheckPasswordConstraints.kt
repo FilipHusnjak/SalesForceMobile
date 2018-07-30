3d45e85fafa60fc03dfccb33768f5da221130be7
LMesaric <luka.mesaric@fer.hr>
2018-07-30 17:54:28 +0200

    Edited strings. Moved data class to more appropriate file.

--- app/src/main/java/hr/atoscvc/salesforcemobile/CheckPasswordConstraints.kt
+++ app/src/main/java/hr/atoscvc/salesforcemobile/CheckPasswordConstraints.kt
@@ -1,29 +1,31 @@
 package hr.atoscvc.salesforcemobile
 
+data class PasswordErrors(val message: String, val success: Boolean)
+
 object CheckPasswordConstraints {
-    fun checkPasswordConstraints(user: String, pass: String): RegisterActivity.PasswordErrors {
+    fun checkPasswordConstraints(user: String, pass: String): PasswordErrors {
         if (pass.length < 8) {
-            return RegisterActivity.PasswordErrors("Password must be at least 8 characters long", false)
+            return PasswordErrors("Password must be at least 8 characters long.", false)
         }
         if (pass.length > 72) {
-            return RegisterActivity.PasswordErrors("Password cannot contain more than 72 characters", false)
+            return PasswordErrors("Password cannot contain more than 72 characters.", false)
         }
         if (user.isNotEmpty() && pass.contains(user, true)) {
-            return RegisterActivity.PasswordErrors("Password must not contain the username", false)
+            return PasswordErrors("Password must not contain the username.", false)
         }
         if (user.contains(pass, true)) {
-            return RegisterActivity.PasswordErrors("Username must not contain the password", false)
+            return PasswordErrors("Username must not contain the password.", false)
         }
         if (!pass.matches(".*[a-z].*".toRegex())) {
-            return RegisterActivity.PasswordErrors("Password must contain at least one lowercase letter", false)
+            return PasswordErrors("Password must contain at least one lowercase letter.", false)
         }
         if (!pass.matches(".*[A-Z].*".toRegex())) {
-            return RegisterActivity.PasswordErrors("Password must contain at least one uppercase letter", false)
+            return PasswordErrors("Password must contain at least one uppercase letter.", false)
         }
         if (!pass.matches(".*\\d.*".toRegex())) {
-            return RegisterActivity.PasswordErrors("Password must contain at least one digit", false)
+            return PasswordErrors("Password must contain at least one digit.", false)
         }
 
-        return RegisterActivity.PasswordErrors("Password is fine.", true)
+        return PasswordErrors("Password is fine.", true)
     }
 }
