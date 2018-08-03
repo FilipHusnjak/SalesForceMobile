34ebacdf020090c1362b4b97030a6ffd6648e1af
LMesaric <luka.mesaric@fer.hr>
2018-08-03 11:28:51 +0200

    Basic functionality for Main Menu buttons.

--- app/src/main/java/hr/atoscvc/salesforcemobile/MainMenuActivity.kt
+++ app/src/main/java/hr/atoscvc/salesforcemobile/MainMenuActivity.kt
@@ -8,7 +8,6 @@ import android.support.v7.app.AppCompatActivity
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main_menu.*
 
-
 class MainMenuActivity : AppCompatActivity() {
 
     private lateinit var userSession: SessionManager
@@ -64,8 +63,28 @@ class MainMenuActivity : AppCompatActivity() {
         }
     }
 
-    //TODO Method should be moved elsewhere
-    //TODO In Manifest: android:parentActivityName=".MainMenuActivity" should be changed accordingly
+    fun onAddContact(@Suppress("UNUSED_PARAMETER") view: View) {
+        val intent = Intent(this, ContactEditorActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun onViewContacts(@Suppress("UNUSED_PARAMETER") view: View) {
+        val intent = Intent(this, ContactListActivity::class.java)
+        startActivity(intent)    }
+
+    fun onAddCompany(@Suppress("UNUSED_PARAMETER") view: View) {
+        val intent = Intent(this, CompanyEditorActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun onViewCompanies(@Suppress("UNUSED_PARAMETER") view: View) {
+        val intent = Intent(this, CompanyListActivity::class.java)
+        startActivity(intent)
+    }
+
+
+    //FIXME Method should be moved elsewhere
+    //FIXME In Manifest: android:parentActivityName=".MainMenuActivity" should be changed accordingly
     fun onChangePassword(@Suppress("UNUSED_PARAMETER") view: View) {
         val intent = Intent(this, ChangePasswordActivity::class.java)
         startActivity(intent)
